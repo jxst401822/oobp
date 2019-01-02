8222f65d0c687995407061a86599ee4bc57e42b0
John.Wang <yunhwang@thought.com>
2019-01-02 16:50:38 +0800

    Init commit about smartBoy

---
+++ src/main/java/com/thoughworks/oobootcamp/parkinglot/SmartParkingBoy.java
@@ -0,0 +1,10 @@
+package com.thoughworks.oobootcamp.parkinglot;
+
+import java.util.ArrayList;
+
+public class SmartParkingBoy extends ParkingBoy{
+
+  public SmartParkingBoy(ArrayList<ParkingLot> parkingLots) {
+    super(parkingLots);
+  }
+}
---
+++ src/test/java/com/thoughworks/oobootcamp/parkinglot/SmartParkingBoyTest.java
@@ -0,0 +1,42 @@
+package com.thoughworks.oobootcamp.parkinglot;
+
+import org.junit.jupiter.api.Test;
+
+import static com.google.common.collect.Lists.newArrayList;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class SmartParkingBoyTest {
+
+  @Test
+  void should_return_ticket_when_smart_boy_parking_car_given_two_same_available_space_parking_lots() {
+    ParkingLot firstParkingLot = new ParkingLot(1);
+    ParkingLot secondParkingLot = new ParkingLot(1);
+    SmartParkingBoy smartParkingBoy = new SmartParkingBoy(newArrayList(firstParkingLot, secondParkingLot));
+    Car myCar = new Car();
+    Ticket ticket =  smartParkingBoy.parkCar(myCar);
+    assertNotNull(ticket);
+    assertSame(myCar,firstParkingLot.pickCar(ticket));
+  }
+
+  @Test
+  void should_return_ticket_when_smart_boy_parking_car_given_the_first_lot_has_more_available_space() {
+    ParkingLot firstParkingLot = new ParkingLot(2);
+    ParkingLot secondParkingLot = new ParkingLot(1);
+    SmartParkingBoy smartParkingBoy = new SmartParkingBoy(newArrayList(firstParkingLot, secondParkingLot));
+    Car myCar = new Car();
+    Ticket ticket =  smartParkingBoy.parkCar(myCar);
+    assertNotNull(ticket);
+    assertSame(myCar,firstParkingLot.pickCar(ticket));
+  }
+
+  @Test
+  void should_return_ticket_when_smart_boy_parking_car_given_the_second_lot_has_more_available_space() {
+    ParkingLot firstParkingLot = new ParkingLot(1);
+    ParkingLot secondParkingLot = new ParkingLot(2);
+    SmartParkingBoy smartParkingBoy = new SmartParkingBoy(newArrayList(firstParkingLot, secondParkingLot));
+    Car myCar = new Car();
+    Ticket ticket =  smartParkingBoy.parkCar(myCar);
+    assertNotNull(ticket);
+    assertSame(myCar,secondParkingLot.pickCar(ticket));
+  }
+}
